591425867d007f825daa948acc78206ecfb9165c
Emine Shaban <emineshaban@icloud.com>
2023-09-20 09:26:13 +0300

    .

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+
+        int[] nums = Arrays
+                .stream(sc
+                        .nextLine()
+                        .split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+
+
+        for (int i = 0; i < nums.length; i++) {
+             stack.push(nums[i]);
+        }
+
+
+
+        for (int i = 0; i <= nums.length; i++) {
+            System.out.println(stack.pop());
+
+        }
     }
 }
