b1166e4cd1c169aabf3c4cc7896cc41a8fefb392
Emine Shaban <emineshaban@icloud.com>
2023-09-20 14:02:55 +0300

    .

---
+++ src/Main5.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+
+public class Main5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Stack<String> stack = new Stack<String>();
+
+        }
+}
